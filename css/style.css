88f39f6115cc2396ae11e77e9087c7931b4a342b
KARNAL-PROJECTS <seraza934@gmail.com>
2026-03-13 01:08:19 +0530

    Create style.css

---
+++ css/style.css
@@ -0,0 +1,12 @@
+<span id="typing"></span>
+
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+<script>
+new Typed("#typing",{
+strings:["Web Designer","Frontend Developer","UI Creator"],
+typeSpeed:60,
+backSpeed:40,
+loop:true
+})
+</script>
